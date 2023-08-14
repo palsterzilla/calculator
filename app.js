1c00188501fbac3a75a0aea0b54c01f042de68ce
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-14 20:59:19 +0700

    Implement multiple input calculation

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
 const buttons = document.querySelectorAll(".btn");
+const para = document.createElement("p")
 let numbers = [];
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+let displayValue = ""
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     numbers.push(button.value)
+    
+    if (/\w/.test(button.value)) {
+      if (button.value === "ac") {
+        allClear()
+        return
+      }
+      displayValue += button.value
+      para.textContent = displayValue
+      document.body.appendChild(para)
 
-    if (button.value === "+") {
-      firstNum = Number(numbers.slice(0, numbers.indexOf("+")).join(""))
-      operator = "+"
-
-    } else if (button.value === "=") {
-      secondNum = Number(numbers.slice((numbers.indexOf("+") + 1), -1).join(""))
-      console.log(math_it_up[operator](firstNum, secondNum))
-      
-    } else if (button.value === "ac") {
-      numbers = []
-      firstNum = ""
-      secondNum = "" 
-      operator = ""
+    } else if (/\W/.test(button.value)) {
+      displayValue = "";
     }
 
+    if (numbers.filter(x => x == "+").length > 1) {
+      firstNum = numbers.slice(0, numbers.indexOf("+")).join("");
+      secondNum = numbers.slice((numbers.indexOf("+") + 1), -1).join("");
+      
+      firstNum = calculate["+"](+firstNum, +secondNum);
+      secondNum = "";
+      para.textContent = firstNum;
+      numbers = numbers.slice(-1);
+      numbers.unshift(firstNum);
+
+    } else if (button.value === "=") {
+      firstNum = numbers.slice(0, numbers.indexOf("+")).join("");
+      secondNum = numbers.slice((numbers.indexOf("+") + 1), -1).join("");
+      firstNum = calculate["+"](+firstNum, +secondNum);
+      secondNum = "";
+      para.textContent = firstNum;
+
+    }
   })
 })
 
-const math_it_up = {
+function allClear() {
+  numbers = [];
+  firstNum = "";
+  secondNum = ""; 
+  operator = "";
+  displayValue = "";
+  para.textContent = ""
+}
+
+const calculate = {
   '+': function (x, y) { return x + y },
-  '-': function (x, y) { return x - y }
+  '-': function (x, y) { return x - y },
+  '*': function (x, y) { return x * y },
+  '/': function (x, y) { return x / y },
 };
