22d366b986d75697813e3ce2482c1781de79da86
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-15 07:26:51 +0700

    Add variable assignment for operator

--- app.js
+++ app.js
@@ -19,27 +19,37 @@ buttons.forEach(button => {
       para.textContent = displayValue
       document.body.appendChild(para)
 
-    } else if (/\W/.test(button.value)) {
+    } else if (/\W/.test(button.value) && button.value !== "=") {
       displayValue = "";
+      operator = numbers.at(-1)
+
     }
 
-    if (numbers.filter(x => x == "+").length > 1) {
+    if (numbers.filter(x => /\W/.test(x)).length > 1 && button.value !== "=") {
       firstNum = numbers.slice(0, numbers.indexOf("+")).join("");
       secondNum = numbers.slice((numbers.indexOf("+") + 1), -1).join("");
       
-      firstNum = calculate["+"](+firstNum, +secondNum);
+      firstNum = calculate[operator](+firstNum, +secondNum);
       secondNum = "";
       para.textContent = firstNum;
+
       numbers = numbers.slice(-1);
       numbers.unshift(firstNum);
-
+      console.log("from if")
+      
     } else if (button.value === "=") {
+      console.log("from else if")
       firstNum = numbers.slice(0, numbers.indexOf("+")).join("");
       secondNum = numbers.slice((numbers.indexOf("+") + 1), -1).join("");
-      firstNum = calculate["+"](+firstNum, +secondNum);
+      
+      firstNum = calculate[operator](+firstNum, +secondNum);
       secondNum = "";
       para.textContent = firstNum;
-
+      
+      numbers = []
+      numbers.unshift(firstNum);
+      operator = ""
+      
     }
   })
 })
